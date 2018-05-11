0e12445246ef10e52bcb02820240e8229cc2dee7
KeithHochevar <36213289+KeithHochevar@users.noreply.github.com>
2018-05-11 06:09:32 -0400

    Add files via upload

---
+++ FinalProject/FinalProject/Hero.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProject
+{
+    class Hero
+    {
+        private string Alterego;
+        private string Weakness;
+        private bool Antihero;
+        private string Gender;
+        private string Race;
+
+        public string Power;
+        public int PowerLevel;
+        public double Health;
+        public string Weapons;
+        public int Age;
+        public string Name;
+        public string nemesis;
+
+        public Hero()
+        {
+        }
+
+        // TODO: Problem 1 - add a constructor method with a different number and/or types of parameters
+        public Hero(string weapons, string weakness, int age)
+        {
+            this.Weapons = weapons;
+            this.Weakness = weakness;
+            this.Age = age;
+        }
+
+        public Hero(string name, string nemesis, string race, string power)
+        {
+            this.Name = name;
+            this.nemesis = nemesis;
+            this.Race = race;
+            this.Power = power;
+        }
+
+        public Hero(string name, string nemesis, string race, string power, bool antihero)
+        {
+            this.Name = name;
+            this.nemesis = nemesis;
+            this.Race = race;
+            this.Power = power;
+            this.Antihero = antihero;
+        }
+
+        public void FightCrime(string address)
+        {
+            // TODO: Problem 2 - Output "Successfully fought crime at address" if PowerLevel > 49
+            if (PowerLevel > 49)
+            {
+                Console.WriteLine("Successfully fought crime at " + address);
+            }
+            else
+            {
+                Console.WriteLine("Failed to fight crime at " + address);
+            }
+        }
+
+        public bool Investigate()
+        {
+            // TODO: Problem 3 - output "Investigated Crime" and return true
+            Console.WriteLine("Investigated Crime");
+            return true;
+            /*
+            bool returnValue = true;
+            if (true)
+            {
+                Console.WriteLine("Investigated Crime");
+            }
+            return returnValue;
+            */
+        }
+
+        public void Fly()
+        {
+            // TODO: Problem 4 - if Power contains Fly, then output "Name is Flying!" else output "Name can't fly!"
+            if (this.Power.Contains("Fly"))
+            {
+                Console.WriteLine(Name + " is Flying!");
+            }
+            else
+            {
+                Console.WriteLine(Name + " can't fly!");
+            }
+        }
+
+        public void Craft(string material, int numMaterials)
+        {
+            Console.WriteLine("Crafting with " + material);
+        }
+
+        public void Climb(int height)
+        {
+            Console.WriteLine(this.Name + " is climbing");
+        }
+
+        public int ChargePower()
+        {
+            // TODO: Problem 5 - increase this.PowerLevel by 1
+            // for (int i = 0; i < 2; i++)
+            // {
+            //      i++;
+            // }
+            // this.PowerLevel++;
+            PowerLevel++;
+            return PowerLevel;
+        }
+
+        // TODO: Problem 6 - Add an overloaded method ChargePower that has a parameter for increaseAmount.  Add increaseAmount to this.PowerLevel and return the new value for this.PowerLevel (try to use a loop calling the other ChargePower)
+        public int ChargePower(int increaseAmount)
+        {
+            for (int i = 0; i < increaseAmount; i++)
+            {
+                this.ChargePower();
+            }
+            return this.PowerLevel;
+        }
+    }
+}
---
+++ FinalProject/FinalProject/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FinalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Problem 7:
+                Each Hero must:
+                 - ChargePower (as much as you want)
+                 - Investigate
+                 - FightCrime (at least one must win. at least one must not win)
+                 - Fly (at least one superhero should be able to fly)
+                 - Climb
+            */
+
+            Hero myHero = new Hero("Sword", "Darkness", 23);
+            myHero.Name = "Superman";
+            myHero.Power = "Strength, Speed, Fly";
+            myHero.ChargePower(200);
+            myHero.Investigate();
+            myHero.FightCrime("1234 Earth");
+            myHero.Fly();
+            myHero.Climb(100);
+
+            Hero myOtherHero = new Hero("Flash", "Zoom", "Human", "Speed", true);
+            myOtherHero.ChargePower(30);
+            myOtherHero.Investigate();
+            myOtherHero.FightCrime("Sunny Street");
+            myOtherHero.Fly();
+            myOtherHero.Climb(8);
+
+            Hero thirdHero = new Hero("Wonder Woman", "All bad people", "Amazonian", "Stength, Speed, Fly");
+            thirdHero.ChargePower(400);
+            thirdHero.Investigate();
+            thirdHero.FightCrime("Baker Street");
+            thirdHero.Fly();
+            thirdHero.Climb(9);
+        }
+    }
+}
